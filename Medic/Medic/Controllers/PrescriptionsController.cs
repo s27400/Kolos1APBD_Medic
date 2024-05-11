7d36cb9663421e3633add866a025b45a3732168c
s27400 <s27400@pjwstk.edu.pl>
2024-05-11 20:00:59 +0200

    Transaction

--- Medic/Medic/Controllers/PrescriptionsController.cs
+++ Medic/Medic/Controllers/PrescriptionsController.cs
@@ -1,6 +1,8 @@
+using System.Transactions;
 using Medic.Models;
 using Medic.Reposiotries;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Writers;
 
 namespace Medic.Controllers;
 
@@ -24,21 +26,36 @@ public class PrescriptionsController : ControllerBase
     [HttpPost]
     public async Task<IActionResult> AddPrescription(PrescriptionDTO newPrescription)
     {
-        if (!await _prescriptionRepository.IsPatientExists(newPrescription.IdPatientDto))
+        try
         {
-            return NotFound($"Pacjent o id : {newPrescription.IdPatientDto} nie istnieje w bazie danych");
+            using (TransactionScope scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
+            {
+                if (!await _prescriptionRepository.IsPatientExists(newPrescription.IdPatientDto))
+                {
+                    return NotFound($"Pacjent o id : {newPrescription.IdPatientDto} nie istnieje w bazie danych");
+                }
+
+                if (!await _prescriptionRepository.IsDoctorExists(newPrescription.IdDoctorDto))
+                {
+                    return NotFound($"Doktor o id : {newPrescription.IdDoctorDto} nie istnieje w bazie danych");
+                }
+
+                if (!_prescriptionRepository.DateVeryfivation(DateOnly.FromDateTime(newPrescription.DateDto),
+                        DateOnly.FromDateTime(newPrescription.DueDateDto)))
+                {
+                    return NotFound("Data wystawienia jest większa niż data ważności recepty");
+                }
+
+                Prescription res = await _prescriptionRepository.AddPrescription(newPrescription);
+                scope.Complete();
+                return Ok(res);
+
+            }
+        }
+        catch (Exception e)
+        {
+            return NotFound("SQL error transaction rollback");
         }
 
-        if (!await _prescriptionRepository.IsDoctorExists(newPrescription.IdDoctorDto))
-        {
-            return NotFound($"Doktor o id : {newPrescription.IdDoctorDto} nie istnieje w bazie danych");
-        }
-
-        if (!_prescriptionRepository.DateVeryfivation(DateOnly.FromDateTime(newPrescription.DateDto), DateOnly.FromDateTime(newPrescription.DueDateDto)))
-        {
-            return NotFound("Data wystawienia jest większa niż data ważności recepty");
-        }
-
-        return Ok(await _prescriptionRepository.AddPrescription(newPrescription));
     }
 }
